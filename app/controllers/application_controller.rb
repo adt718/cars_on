5f2017bd552de4b6b07c56a7c8c210615a9069d0
adt718 <ksgses531@gmail.com>
2021-05-10 11:45:49 +0000

    add genres and cart_items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_customer!
+
+
+  def after_sign_in_path_for(resource)
+    mypage_root_path # ログイン後に遷移するpathを設定
+  end
+
+  def after_sign_out_path_for(resource)
+    new_customer_session_path # ログアウト後に遷移するpathを設定
+  end
 
   protected
 
